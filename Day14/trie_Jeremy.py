27e1d2eae43e70daa7f3fb511eb360ddfe90743d
jeremyimmanuel <jeremytandjung@icloud.com>
2020-05-14 23:52:36 -0700

    added day14 sol -jeremy

--- Day14/trie_Jeremy.py
+++ Day14/trie_Jeremy.py
@@ -1,113 +1,68 @@
-from typing import List, Dict
 class TrieNode:
-    def __init__(self, char: str = '', children = [], endOfInsertedWord: bool = False):
-        self.char = char
-        self.children = children
-        self.endOfInsertedWord = endOfInsertedWord
-    
-    @property
-    def childrenLetters(self):
-        ret = []
-        for child in self.children:
-            ret.append(child.val)
-        return ret
-
-    def getChild(self, val: str):
-        for child in self.children:
-            if child.val == val:
-                return child
-        return None
+    def __init__(self):
+        self.isWord = False
+        self.children = {}
 
 class Trie:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = TrieNode()
-        
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
-        # maybe exists?
-        maybe = self.getLastNode(word)
-        if maybe:
-            maybe.endOfInsertedWord = True
-        else:
-            self.insertHelper(word)
-
-    def insertHelper(self, word: str):
         curr = self.root
-        for i in range(len(word)):
-            char = word[i]
-            if char in curr.childrenLetters:
-                curr = curr.getChild(char)
-            else:
-                newNode = TrieNode(char=char)
-                curr.childrenLetters.append(newNode)
-                curr = curr.getChild(char)
-        
-        curr.endOfInsertedWord = True
-                
-
-
-
-        
-    def getLastNode(self, word: str) -> TrieNode:
-        '''
-        If word exists (startsWith)
-        return last node the word
-        else
-        return None
-        '''
-        if self.startsWith(word):
-            curr = self.root
-            for i in range(len(word)):
-                curr = curr.getChild(word[i])
-            return curr
-        else:
-            return None
-        
-
+        for i in word:
+            if i not in curr.children:
+                curr.children[i] = TrieNode()
+            curr = curr.children[i]
+        curr.isWord = True
             
-        
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-        return self.searchHelper(self.root, word, 0)
-    
+        curr = self.root
+        for i in word:
+            if i not in curr.children:
+                return False
+            curr = curr.children[i]
+        return curr.isWord
+
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
-        return searchHelper(self.root, prefix, 0, False)
-        
-    def searchHelper(self, curr: TrieNode, target: str, index: int, isSearch: bool = True) -> bool:
-        # last iteration
-        if index == len(target) - 1:
-            if isSearch:
-                return curr.endOfInsertedWord
-            else:
-                return True
+        curr = self.root
+        for i in word:
+            if i not in curr.children:
+                return False
+            curr = curr.children[i]
+        return True
 
-        
-        # look for next char in
-        char = target[index]
-        if char in curr.childrenLetters:
-            return self.searchHelper(curr.getChild(char), target, index+1, isSearch)
-        
-        # if the next char is not even in the children of curr 
-        # then word doesn't exist
-        return False
+###########
+# Fastest sol in py (104ms)
 
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
+class Trie2:
+    def __init__(self):
+        self.trie = {}
 
-if __name__ == "__main__":
-    Trie()
+    def insert(self, word: str) -> None:
+        root = self.trie
+        for c in word:
+            if c not in root:
+                root[c] = {}
+            root = coot[c]
+        root['#'] = '#'
+    
+    def search(self, word: str) -> bool:
+        return self.startsWith(word + '#')
+    
+    def startsWith(self, prefix: str) -> bool:
+        start = self.trie
+        for c in prefix:
+            if c not in start:
+                return False
+            start = start[c]
+        return True
