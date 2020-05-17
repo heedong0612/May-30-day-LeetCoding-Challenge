9d0358e98f22e5628ae9253c4ded541d97e2123c
Lee Donghee <heedong0612@gmail.com>
2020-05-17 15:50:16 +0900

    uploaded Donghee's Day 16 solution

---
+++ Day16/oddEvenLinkedList_Donghee.cpp
@@ -0,0 +1,37 @@
+// Donghee Lee
+// 5/16/2020
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+        ListNode* odd = head;
+        
+        while(even != nullptr && even->next != nullptr) {
+            //cout << "odd: " << odd->val << " , even: " << even->val << endl;
+            
+            odd->next = even->next;
+            odd = odd->next;
+            
+            even->next = odd->next;
+            even = even->next;
+        }
+        
+        odd->next = evenHead;
+        
+        return head;
+    }
+};
