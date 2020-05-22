cfe9f4f167f33b8d234ef82a1336b1f160013e2e
Lee Donghee <heedong0612@gmail.com>
2020-05-23 08:28:34 +0900

    uploaded Donghee's Day22 python solution

--- Day22/sortCharactersByFrequency_Donghee.py
+++ Day22/sortCharactersByFrequency_Donghee.py
@@ -1,3 +1,5 @@
+import sys
+
 class Solution(object):
     def frequencySort(self, s):
         """
@@ -5,3 +7,48 @@ class Solution(object):
         :rtype: str
         """
         
+        counts = [0 for x in range(256)]
+
+        for c in s:
+            counts[ord(c)] += 1
+
+        mp = dict() # mp = { 'a' : 4, 'b' : 2 }
+
+        maxCount = 0
+        minCount = sys.maxsize # INT_MAX
+
+        for i in range(256) :
+            if counts[i] > 0 :
+                if counts[i] not in mp :
+                    mp[counts[i]] = ""
+                
+                mp[counts[i]] += chr(i)
+                
+                maxCount = max(maxCount, counts[i])
+                minCount = min(minCount, counts[i])
+        
+        ans = ""
+        
+        # range(start, stop, step)
+        for i in range(maxCount, minCount - 1, -1) :
+            # if (mp.find(i) != mp.end()) {
+            if i in mp : 
+                for ch in mp[i] :
+                    for j in range(i) :
+                        ans += ch
+
+        return ans
+
+            
+    # from collections import defaultdict as dd
+    # a["a"]; error
+    # a['a']; give you a fallback
+    # c = Counter('Jeremy')
+    # c['e'] -> 2
+    # c['a'] -> 0
+    # contacts = {}
+    # contacts['Donghee'] -> Person()
+
+        
+
+
